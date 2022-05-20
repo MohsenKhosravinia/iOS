353bc494d0bc7775f1447873f3d63e30805313ef
Mohsen Khosravinia <khosravinia.mohsen@gmail.com>
2022-05-20 21:44:47 +0430

    Remove the Flickr project from this repo.

--- FlickrFetcher/Pods/Target Support Files/Pods-FlickerFetcher-FlickerFetcherUITests/Pods-FlickerFetcher-FlickerFetcherUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlickerFetcher_FlickerFetcherUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FlickerFetcher_FlickerFetcherUITests
-@end
--- FlickrFetcher/Pods/Target Support Files/Pods-FlickerFetcherTests/Pods-FlickerFetcherTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FlickerFetcherTests : NSObject
-@end
-@implementation PodsDummy_Pods_FlickerFetcherTests
-@end
