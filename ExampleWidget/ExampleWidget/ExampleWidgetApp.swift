4e5dd9f16876c33a9611f0e3330de633f0d7b00e
Mohsen Khosravinia <khosravinia.mohsen@gmail.com>
2022-12-12 16:50:37 +0330

    Add a widget kit example

---
+++ ExampleWidget/ExampleWidget/ExampleWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleWidgetApp.swift
+//  ExampleWidget
+//
+//  Created by Mohsen Khosravinia on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
